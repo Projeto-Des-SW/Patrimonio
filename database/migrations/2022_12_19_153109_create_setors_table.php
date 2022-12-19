da5839dd14ad146038ccbf9f7b2c84086b5e1f76
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-19 12:54:46 -0300

    Criação de model e migration de setor

---
+++ database/migrations/2022_12_19_153109_create_setors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('setors', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('nome');
+            $table->string('codigo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('setors');
+    }
+};
