2e75e4fbb245392b92a81f9c1b6d6aa2d67113aa
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-19 14:28:40 -0300

    Criação de seeder e factory de cargo

---
+++ database/seeders/CargoSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cargo;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CargoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Cargo::factory(1)->create(['nome' => 'Professor']);
+        Cargo::factory(1)->create(['nome' => 'Coordenador']);
+    }
+}
