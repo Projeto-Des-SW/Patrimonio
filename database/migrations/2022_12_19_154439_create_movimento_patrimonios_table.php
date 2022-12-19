56fd572fa27e514593f2797c8a72f206394a74dc
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-19 12:52:06 -0300

    Criação do model e migration de movimento_patrimonio

---
+++ database/migrations/2022_12_19_154439_create_movimento_patrimonios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movimento_patrimonios', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->unsignedBigInteger('movimento_id');
+            $table->foreign('movimento_id')->references('id')->on('movimentos');
+
+            $table->unsignedBigInteger('patrimonio_id');
+            $table->foreign('patrimonio_id')->references('id')->on('patrimonios');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movimento_patrimonios');
+    }
+};
