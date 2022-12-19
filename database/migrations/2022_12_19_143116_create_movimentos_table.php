8fc4b234fe6485c6edd6f2227293a555451a90a8
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-19 12:55:50 -0300

    Criação de model e migration de movimento

---
+++ database/migrations/2022_12_19_143116_create_movimentos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movimentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('observacao')->nullable();
+
+            $table->unsignedInteger('servidor_destino_id');
+            $table->foreign('servidor_destino_id')->references('id')->on('servidors');
+
+            $table->unsignedInteger('servidor_origem_id');
+            $table->foreign('servidor_origem_id')->references('id')->on('servidors');
+
+
+            $table->unsignedInteger('tipo_movimento_id');
+            $table->foreign('tipo_movimento_id')->references('id')->on('tipo_movimentos');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movimentos');
+    }
+};
