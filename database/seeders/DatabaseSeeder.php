3391ea4198cbdc1374fafd1203dc02f6336a3278
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-19 15:13:55 -0300

    Criação de factory e seeder de TipoMovimento

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CargoSeeder::class,
             ServidorSeeder::class,
+            TipoMovimentoSeeder::class,
         ]);
     }
 }
