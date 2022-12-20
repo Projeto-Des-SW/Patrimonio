8c99b33adba7205b0b9f825f9020d84f7f23fd80
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-20 13:49:08 -0300

    Criação de fillable e relações de sala

--- app/Models/Sala.php
+++ app/Models/Sala.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Sala extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome', 'telefone', 'predio_id'];
+
+    public function predio()
+    {
+        return $this->belongsTo(Predio::class);
+    }
+
+    public function patrimonios()
+    {
+        return $this->hasMany(Patrimonio::class);
+    }
 }
