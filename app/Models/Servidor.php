bed9a755131223f6c23dd5bb3a6382e574dee37f
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-20 13:49:26 -0300

    Criação de fillable e relações de servidor

--- app/Models/Servidor.php
+++ app/Models/Servidor.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Servidor extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['cpf', 'matricula', 'user_id', 'cargo_id'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function cargo(){
+        return $this->belongsTo(Cargo::class);
+    }
 }
