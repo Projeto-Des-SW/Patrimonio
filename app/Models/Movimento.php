a828d778a8c6a17067c237241de5056cf3497b3d
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-20 13:21:53 -0300

    Criando fillable e relações de movimento

--- app/Models/Movimento.php
+++ app/Models/Movimento.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Movimento extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['observacao', 'servidor_destino_id', 'servidor_origem_id', 'tipo_movimento_id'];
+
+    public function servidor_destino(){
+        return $this->belongsTo(Servidor::class, 'servidor_destino_id');
+    }
+
+    public function servidor_origem(){
+        return $this->belongsTo(Servidor::class, 'servidor_origem_id');
+    }
+
+    public function tipo_movimento(){
+        return $this->belongsTo(TipoMovimento::class);
+    }
 }
