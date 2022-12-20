1735334b7e0831481be5cfb9c476bc0212e36768
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-20 13:50:10 -0300

    Criação de fillable e relações de tipo_usuario

--- app/Models/TipoUsuario.php
+++ app/Models/TipoUsuario.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class TipoUsuario extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome'];
+
+    public function users(){
+        return $this->hasMany(User::class);
+    }
 }
