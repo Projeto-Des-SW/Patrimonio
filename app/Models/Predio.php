aff45ed6ddc104d4a9668c259c7faa870117e760
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-20 13:48:55 -0300

    Criação de fillable e relações de predio

--- app/Models/Predio.php
+++ app/Models/Predio.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Predio extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome'];
+
+    public function salas(){
+        return $this->hasMany(Sala::class);
+    }
 }
