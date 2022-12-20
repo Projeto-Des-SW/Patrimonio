491dbb1a2558d462abea0f264a678d4521072876
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-20 13:49:41 -0300

    Criação de fillable e relações de setor

--- app/Models/Setor.php
+++ app/Models/Setor.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Setor extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome', 'codigo', 'setor_pai_id'];
+
+    public function setor_pai(){
+        return $this->belongsTo(Setor::class, 'setor_pai_id');
+    }
 }
