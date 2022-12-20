303ab1e55e8f8a3b81de1f93a8fa6f9bd699ceda
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-20 13:48:41 -0300

    Criação de fillable e relações de patrimonio

--- app/Models/Patrimonio.php
+++ app/Models/Patrimonio.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Patrimonio extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome', 'tipo', 'nota_fiscal', 'descricao', 'servidor_id', 'classificacao_id', 'origem_id', 'sala_id', 'situacao_id'];
+
+    public function servidor()
+    {
+        return $this->belongsTo(Servidor::class);
+    }
+
+    public function classificacao(){
+        return $this->belongsTo(Classificacao::class);
+    }
+
+    public function origem(){
+        return $this->belongsTo(Origem::class);
+    }
+
+    public function sala(){
+        return $this->belongsTo(Sala::class);
+    }
+
+    public function situacao()
+    {
+        return $this->belongsTo(Situacao::class);
+    }
+
+    public function codigos()
+    {
+        return $this->hasMany(Codigo::class);
+    }
 }
