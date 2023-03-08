81b28683ffb2958f01d77df3de35a642c2d95aaa
Gust4voSales <manoel0gustavo@gmail.com>
2023-03-08 20:56:43 -0300

    feat: add header

--- resources/views/predio/create.blade.php
+++ resources/views/predio/create.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    <h3 class="text-center">Cadastro de Prédio</h3>
+    @include('layouts.components.header', ['page_title' => 'Criar Prédio', 'back' => true])
+
     <form method="POST" action="{{route('predio.store')}}" enctype="multipart/form-data">
         @csrf
         @include('predio.form')
         <div class="row mt-4">
             <div class="col-3">
-                <a class="btn btn-secondary w-100" href="{{route('predio.index')}}">Voltar</a>
-            </div>
-            <div class="col-3 offset-6">
                 <button type="submit" class="btn btn-success w-100">Cadastrar</button>
             </div>
         </div>
