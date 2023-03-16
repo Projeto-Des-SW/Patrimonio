b0820d998800375a905bd061de8946a98e2ec0ed
Edgarvital <edgar.tecnico@hotmail.com>
2023-03-16 20:26:31 -0300

    Criando tela para adicionar código ao patrimônio

--- database/migrations/2022_12_19_154955_create_codigos_table.php
+++ database/migrations/2022_12_19_154955_create_codigos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('codigos', function (Blueprint $table) {
+        Schema::create('codigo', function (Blueprint $table) {
             $table->id();
             $table->string('codigo')->unique();
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('codigos');
+        Schema::dropIfExists('codigo');
     }
 };
