d6b82751cb129a43c0fa12a2d3d6324131fd0d7a
ThaysonScript <thaysonguedes01@gmail.com>
2023-12-06 13:27:28 -0300

    concerto de tipo de usuario ao tentar registrar novo usuario

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -75,9 +75,11 @@ class RegisterController extends Controller
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'tipo_usuario_id' => 2,
+            // 'tipo_usuario_id' => 2,
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->tipo_usuarios()->sync(2);
 
         Servidor::create(['user_id' => $user->id,
             'matricula' => $data['matricula'],
