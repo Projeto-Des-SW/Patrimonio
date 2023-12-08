e41dde498e5be270fb3f4b66a29d97a68ef3cff0
ThaysonScript <thaysonguedes01@gmail.com>
2023-12-08 14:55:40 -0300

    criado metodo para verificar se usuario é admin ou servidor

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
         return $this->hasOne(Servidor::class)
             ->withTrashed();
     }
+
+    public function hasAnyRoles($tipo)
+    {
+        return $this->tipoUsuarios()->whereIn('nome', $tipo)->exists();
+    }
 }
