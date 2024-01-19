12766c3cb008c7bebd4b79d8f3ec1665f6a3fbf5
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-01-19 17:35:13 -0300

    Finalizacao da aplicacao do design na  maioria das tabela

--- resources/views/predio/index.blade.php
+++ resources/views/predio/index.blade.php
@@ -3,43 +3,41 @@
     <div class="row">
     @include('layouts.components.header', ['page_title' => 'Prédios', 'back' => false])
     </div>
-    <table class="table table-hover table-responsive mx-2 mt-4" id="predio_table">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Data de Criação</th>
-            <th class="text-center" scope="col">Ações</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($predios as $predio)
+    <div class="container">
+        <table class="table table-hover shadow-lg" style="border-radius: 10px; overflow:hidden; " >
+            <thead class="text-md-center" style="background-color: #1A2876; color: white;">
             <tr>
-                <td>{{$predio->id}}</td>
-                <td>{{$predio->nome}}</td>
-                <td>{{$predio->created_at}}</td>
-                <td class="text-center d-flex justify-content-around">
-                    <a class="btn btn-primary rounded-circle d-flex justify-content-center align-items-center action-button" href="{{route('predio.edit', ['predio_id' => $predio->id])}}">
-                        <img src="{{URL::asset('/assets/edit_icon.svg')}}" width="15px" alt="Icon de edição">
-                    </a>
-
-                    <form action="{{route('predio.delete', ['predio_id' => $predio->id])}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        
-                        <button class="btn btn-danger rounded-circle d-flex justify-content-center align-items-center action-button" href="#" type="submit">
-                            <img src="{{URL::asset('/assets/delete.svg')}}" width="20px" alt="Icon de remoção">
-                        </button>
-                    </form>
-
-                    <a class="btn btn-primary rounded-circle d-flex justify-content-center align-items-center action-button" href="{{route('sala.index', ['predio_id' => $predio->id])}}">
-                        <img src="{{URL::asset('/assets/salas.svg')}}" width="20px" alt="Icon de salas">
-                    </a>
-            </td>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Data de Criação</th>
+                <th>Ações</th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach($predios as $predio)
+                <tr class="text-md-center">
+                    <td class="py-4">{{$predio->id}}</td>
+                    <td class="py-4">{{$predio->nome}}</td>
+                    <td class="py-4">{{$predio->created_at}}</td>
+                    <td class="py-4">
+                        <div>
+                            <a class="" href="{{route('predio.edit', ['predio_id' => $predio->id])}}">
+                                <img src="{{asset('/images/pencil.png')}}" width="24px" alt="Icon de edição">
+                            </a>
+                            <a href="{{route('predio.delete', ['predio_id' => $predio->id])}}">
+                            
+                                <img src="{{asset('/images/delete.png')}}" width="24px" alt="Icon de remoção">
+                            
+                            <a class="" href="{{route('sala.index', ['predio_id' => $predio->id])}}">
+                                <img src="{{asset('/images/Vector.png')}}" width="19px" alt="Icon de salas">
+                            </a>
+                        </div>
+                </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
     <div class="col-3">
             <a class="w-100 btn btn-primary" href="{{route('predio.create')}}">Cadastrar</a>
         </div>
--- resources/views/setor/index.blade.php
+++ resources/views/setor/index.blade.php
@@ -11,48 +11,42 @@
         </div>
     </div>
 
-    <table class="table table-hover table-responsive mx-2 mt-4" id="setor_table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Código</th>
-                <th class="text-center" scope="col">Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($setores as $setor)
+    <div class="container">
+        <table class="table table-hover shadow-lg" style="border-radius: 10px; overflow:hidden; ">
+            <thead class="text-md-center" style="background-color: #1A2876; color: white;">
                 <tr>
-                    <td>{{ $setor->id }}</td>
-                    <td>{{ $setor->nome }}</td>
-                    <td>{{ $setor->codigo }}</td>
-                    <td class="text-center d-flex justify-content-around">
-                        <a class="btn btn-primary rounded-circle d-flex justify-content-center align-items-center action-button"
-                            href="{{ route('setor.edit', ['setor_id' => $setor->id]) }}">
-                            <img src="{{ URL::asset('/assets/edit_icon.svg') }}" width="15px" alt="Icon de edição">
-                        </a>
-
-                        <form action="{{ route('setor.delete', ['setor_id' => $setor->id]) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button
-                                class="btn btn-danger rounded-circle d-flex justify-content-center align-items-center action-button"
-                                type="submit">
-                                <img src="{{ URL::asset('/assets/delete.svg') }}" width="20px" alt="Icon de remoção">
-                            </button>
-                        </form>
-
-                        <a class="btn btn-primary rounded-circle d-flex justify-content-center align-items-center action-button"
-                            href="{{ route('setor.index', ['setor_pai_id' => $setor->id]) }}">
-                            <img src="{{ URL::asset('/assets/setores.svg') }}" width="20px" alt="Icon de edição">
-                        </a>
-                    </td>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Código</th>
+                    <th>Ações</th>
                 </tr>
-            @empty
-                <td class="text-center" colspan="4">Ainda não existem setores cadastrados</td>
-            @endempty
-    </tbody>
-</table>
+            </thead>
+            <tbody>
+                @forelse($setores as $setor)
+                    <tr class="text-md-center">
+                        <td class="py-4">{{ $setor->id }}</td>
+                        <td class="py-4">{{ $setor->nome }}</td>
+                        <td class="py-4">{{ $setor->codigo }}</td>
+                        <td class="py-4">
+                            <div>
+                                <a href="{{ route('setor.edit', ['setor_id' => $setor->id]) }}">
+                                    <img src="{{asset('/images/pencil.png')}}" width="24px" alt="Icon de edição">
+                                </a>
+                                <a href="{{ route('setor.delete', ['setor_id' => $setor->id]) }}" method="post">
+                                    <img src="{{asset('/images/delete.png')}}" width="24px" alt="Icon de remoção">
+                                </a>
+                                <a href="{{ route('setor.index', ['setor_pai_id' => $setor->id]) }}">
+                                    <img src="{{asset('/images/vision.png')}}" width="24px" alt="Icon de remoção">
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                    @empty
+                        <td class="text-center" colspan="4">Ainda não existem setores cadastrados</td>
+                    @endempty
+            </tbody>
+        </table>
+    </div>
 @if (isset($setor_pai))
     <div class="row mt-4">
         <div class="col-3">
