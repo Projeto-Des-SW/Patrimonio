231a8aa02223328407fc96d3f31bc0d250bd7fc6
Rener1 <rener.tome123@gmail.com>
2024-01-22 18:48:09 -0300

    Finalizando tela de cadastro de patrimônios. Removendo scripts desnecessarios. Atualizando versão do bootstrap.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,29 +10,22 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- styles-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/layouts/footer.css">
     <link rel="stylesheet" href="../css/layouts/navbar.css">
-    
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
 
     <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"
         integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
-    </script> --}}
-    
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
 
@@ -41,17 +34,20 @@
 
 <body>
     @include('layouts.components.navbar')
-    
-    <main class="p-1 w-100 whitesmoke-background" style="height: 95vh;">
-        
+
+    <main class="p-1 w-100 whitesmoke-background" style="height: 100vh;">
+
         @include('layouts.components.messages')
         @yield('content')
-            
-        
+
+
     </main>
 
     @include('layouts.components.footer')
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- resources/views/patrimonio/create.blade.php
+++ resources/views/patrimonio/create.blade.php
@@ -1,7 +1,151 @@
 @extends('layouts.app')
+
 @section('content')
-    @include('layouts.components.header', ['page_title' => 'Criar patrimônio', 'back' => true])
-    <form method="POST" action="{{route('patrimonio.store')}}" enctype="multipart/form-data">
+    <div class="container mt-5">
+        <div class="mb-4">
+            <div class="row align-items-start">
+                <h1 class="display-5">
+                    <strong>
+                        <a href="#" class="text-decoration-none link-primary">Patrimônio</a>
+                        > Cadastrar patrimônio
+                    </strong>
+                </h1>
+            </div>
+        </div>
+        <div>
+            <form action="route('patrimonio.store')" method="post">
+                <div class="mb-3">
+                    <div class="row">
+                        <div class="col">
+                            <label for="nomeItem" class="form-label">Nome do item:</label>
+                            <input type="text" class="form-control" name="nomeItem" id="nomeItem">
+                        </div>
+                        <div class="col">
+                            <label for="descricao" class="form-label">Descrição:</label>
+                            <input type="text" class="form-control" name="descricao" id="descricao">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <div class="row">
+                        <div class="col">
+                            <label for="classificacao" class="form-label">Classificação:</label>
+                            <select class="form-select" aria-label="Selecione uma classificação" id="classificacao"
+                                name="classificacao">
+                                <option selected>Selecione uma classificação</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label for="origem" class="form-label">Origem:</label>
+                            <select class="form-select" aria-label="Selecione uma Origem" id="origem" name="origem">
+                                <option selected>Selecione uma Origem</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label for="situacao" class="form-label">Situação:</label>
+                            <select class="form-select" aria-label="Selecione uma Situação" id="situacao" name="situacao">
+                                <option selected>Selecione uma Situação</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <div class="row">
+                        <div class="col">
+                            <label for="predio" class="form-label">Predio:</label>
+                            <select class="form-select" aria-label="Default select example" id="predio" name="predio">
+                                <option selected>Selecione um predio</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label for="sala" class="form-label">Sala:</label>
+                            <select class="form-select" aria-label="Default select example" id="sala" name="sala">
+                                <option selected>Selecione uma sala</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label for="servidor" class="form-label">Servidor:</label>
+                            <select class="form-select" aria-label="Default select example" id="servidor" name="servidor">
+                                <option selected>Selecione um servidor</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <div class="row">
+                        <div class="col">
+                            <label for="dataCompra" class="form-label">Data de compra:</label>
+                            <input type="date" class="form-control" name="dataCompra" id="dataCompra">
+                        </div>
+                        <div class="col">
+                            <label for="valorItem" class="form-label">Valor do item:</label>
+                            <input type="number" class="form-control" name="valorItem" id="valorItem">
+                        </div>
+                        <div class="col">
+                            <label for="contaContabil" class="form-label">Conta contábil:</label>
+                            <input type="number" class="form-control" name="contaContabil" id="contaContabil">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <div class="row">
+                        <div class="col">
+                            <label for="empenho" class="form-label">Empenho:</label>
+                            <input type="text" class="form-control" name="empenho" id="empenho">
+                        </div>
+                        <div class="col">
+                            <label for="notaFiscal" class="form-label">Nota fiscal:</label>
+                            <input type="text" class="form-control" name="notaFiscal" id="notaFiscal">
+                        </div>
+                        <div class="col">
+                            <label for="processoLicitacao" class="form-label">Processo de licitação:</label>
+                            <select class="form-select" aria-label="Selecione o processo de licitação"
+                                id="processoLicitacao" name="processoLicitacao">
+                                <option selected>Selecione o processo de licitação</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <div class="row">
+                        <div class="col-3">
+                            <label for="#" class="mb-2">Bem privado?</label>
+                            <div class="form-check d-flex justify-content-between px-0">
+                                <input class="btn-check" type="radio" name="bemPrivado" id="privadoSim">
+                                <label class="btn btn-primary col-5" for="privadoSim">
+                                    Sim
+                                </label>
+                                <input class="btn-check" type="radio" name="bemPrivado" id="privadoNao" checked>
+                                <label class="btn btn-primary col-5" for="privadoNao">
+                                    Não
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <label for="observacoes" class="form-label">Observações pertinentes a este
+                                patrimônio:</label>
+                            <textarea class="form-control" id="observacoes" rows="3"></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <div class="row justify-content-center">
+                        <div class="col-auto">
+                            <button class="btn btn-primary submit">Cadastrar</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    {{-- <form method="POST" action="{{ route('patrimonio.store') }}" enctype="multipart/form-data">
         @csrf
         @include('patrimonio.form')
         <div class="row mt-4">
@@ -9,5 +153,5 @@
                 <button style="max-width: 200px" type="submit" class="btn btn-success w-100">Salvar</button>
             </div>
         </div>
-    </form>
+    </form> --}}
 @endsection
