82d6dd1431996e0b94c979104a5b056654990744
Rener1 <rener.tome123@gmail.com>
2024-03-14 14:22:33 -0300

    Melhorando a aparência do footer.

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,8 +1,8 @@
-<footer class="d-flex mt-auto">
-    <div class="col-4">
-        <h1 class="text-white"><strong>LOGO</strong></h1>
+<footer class="d-flex mt-auto justify-content-between p-3">
+    <div class="col-3 text-center">
+        <h4 class="text-white"><strong>Logo</strong></h4>
     </div>
-    <div class="col-4 text-center">
+    <div class="col-3 text-center">
 
         <a href="http://ufape.edu.br" target="_blank">
             <img src="{{ asset('images/layouts/footer/UFAPElogo.png') }}" alt="" class="logo-ufape">
@@ -11,16 +11,17 @@
             <img src="{{ asset('images/layouts/footer/LMTSlogo.png') }}" alt="" class="logo-lmts">
         </a>
     </div>
-    <div class="col-4 text-center">
-        <a href="">
+    
+    <div class="col-3 text-center">
+        <a href="#">
             <img class="logo-redes" src="{{ asset('images/layouts/footer/email.svg') }}" alt="">
         </a>
-
-        <a href="">
+    
+        <a href="#" class="mx-4">
             <img class="logo-redes" src="{{ asset('images/layouts/footer/facebook.svg') }}" alt="">
         </a>
 
-        <a href="">
+        <a href="#">
             <img class="logo-redes" src="{{ asset('images/layouts/footer/instagram.svg') }}" alt="">
         </a>
     </div>
