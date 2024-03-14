e2f01c4a39732ed1ddc3397b04acd28c410417ac
AnaliceBattisti <analicebattistii@gmail.com>
2024-03-14 19:58:48 -0300

    Ajuste na Tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,20 @@
 @extends('layouts.app')
+@section('content')
 
 <link rel="stylesheet" href="/css/home/login.css">
 
-
-
-@section('content')
-<div class="container teste-t">
-
+<div class="container mt-5">
     <div class="row">
         <section class="col-md-6 d-flex flex-column justify-content-center">
             <h2><strong>Patrimônio</strong></h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+
+            <p class="mt-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                  Labore facere quod voluptatem accusamus at id aliquam iste 
                  quam odio, pariatur alias consequuntur deleniti 
                 consequatur debitis non libero ea explicabo! Adipisci.</p>
         </section>
     
-    
-        
-        <div class="col-md-6 d-flex justify-content-end">
+        <div class="col-md-6 d-flex align-items-center justify-content-end">
             <form class="shadow form-home" method="POST" action="{{ route('login') }}">
                 @csrf
                 <h4 class="text-md-center"><strong>Login</strong></h4>
@@ -33,8 +29,7 @@
                         </span>
                     @enderror
                 </div>
-            
-            
+
                 <div>
                     <label for="password">{{ __('Senha') }}</label>
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -44,14 +39,12 @@
                         </span>
                     @enderror
                 </div>
-            
-            
-        
-                <div>
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                    <label class="form-check-label" for="remember">
-                        {{ __('Lembre-se de mim') }}
-                    </label>
+
+                <div class="sub-password" >
+                    <div>
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="form-check-label" for="remember">{{ __('Lembre-se de mim') }}</label>
+                    </div>
                     @if (Route::has('password.request'))
                         <a class="btn btn-link" href="{{ route('password.request') }}">
                             {{ __('Esqueceu sua senha?') }}
