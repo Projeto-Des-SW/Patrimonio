9ffaa7e3998cbf69a55764e230caa052c3ca56a1
Rener1 <rener.tome123@gmail.com>
2024-03-14 16:42:11 -0300

    Aprimorando aparência e funcionalidades da navbar. View refatorada.

--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -1,48 +1,47 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top p-3">
     <div class="container">
-        <a class="navbar-brand" href="{{route('home')}}">
+        <a class="navbar-brand" href="{{ route('home') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
 
+        <div class="collapse navbar-collapse align-items-center" id="navbarSupportedContent">
+            @auth
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item me-4">
+                        <a class="nav-link @if(Str::startsWith(request()->path(), 'patrimonio')) active @endif" href="{{ route('patrimonio.index') }}">Patrimônio</a>
+                    </li>
+                    <li class="nav-item mx-4">
+                        <a class="nav-link @if(Str::startsWith(request()->path(), 'predio')) active @endif" href="{{ route('predio.index') }}">Prédios</a>
+                    </li>
+                    <li class="nav-item mx-4">
+                        <a class="nav-link @if(Str::startsWith(request()->path(), 'cargo')) active @endif" href="{{ route('cargo.index') }}">Cargos</a>
+                    </li>
+                    <li class="nav-item mx-4">
+                        <a class="nav-link @if(Str::startsWith(request()->path(), 'movimento')) active @endif" href="{{ route('movimento.index') }}">Movimentacões</a>
+                    </li>
+                    <li class="nav-item ms-4">
+                        <a class="nav-link @if(Str::startsWith(request()->path(), 'setor')) active @endif" href="{{ route('setor.index') }}">Setores</a>
+                    </li>
+                </ul>
+                
+                <div class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ms-auto">
-                <!-- Authentication Links -->
-                @guest
-                    
-                @else
-
-                    <div class="nav-link nav-item">
-                        <a href="{{route('patrimonio.index')}}">Patrimônio</a>
-                        <a href="{{route('predio.index')}}">Prédios</a>
-                        <a href="{{route('cargo.index')}}">Cargos</a>
-                        <a href="{{route('classificacao.index')}}">Classificacão Contábil</a>
-                        <a href="{{route('movimento.index')}}">Movimentacões</a>
-                        <a href="{{route('setor.index')}}">Setores</a>
-                    </div>
-
-
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                           data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            {{ __('Sair') }}
                         </a>
 
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Sair') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </ul>
+                </div>
+            @endauth
         </div>
     </div>
 </nav>
