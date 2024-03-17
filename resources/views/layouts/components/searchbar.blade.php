6f428009c103682140c80ad7c2ed6d7656ff8495
Rener1 <rener.tome123@gmail.com>
2024-03-17 12:48:15 -0300

    Searchbar componentizada.

---
+++ resources/views/layouts/components/searchbar.blade.php
@@ -0,0 +1,33 @@
+<div class="my-5 col-md-8 mx-auto">
+    <div class="d-flex align-items-center mb-2">
+        <h3>{{ $title }}</h3>
+        @if (isset($addButton))
+            <a href="{{ $addButton }}" style="margin-left: 5px;">
+                <img src="{{ asset('assets/plus-circle-fill.svg') }}" alt="Ícone de Adição"
+                    style="width: 30px; height: 30px;">
+            </a>
+        @endif
+    </div>
+
+    @if (isset($searchForm))
+        <div class="d-flex justify-content-center align-items-center">
+            <div class="flex-fill">
+                <form class="mb-0" action="{{ $searchForm }}" method="get">
+                    <div class="input-group">
+                        <input class="form-control" type="text" name="busca" id="busca"
+                            placeholder="Pesquisar por nome">
+                        <button style="background-color: #1A2876" class="btn" type="submit">
+                            <img src="{{ asset('images/busca.png') }}" alt="Buscar">
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div class="ms-3">
+                <button style="background-color: transparent; border: none; display: flex; align-items: center;">
+                    <img src="{{ asset('assets/Vector.svg') }}" alt="Ícone de filtro" style="margin-right: 10px;">
+                    <span style="color: #1A2876;">Filtrar</span>
+                </button>
+            </div>
+        </div>
+    @endif
+</div>
--- resources/views/patrimonio/index.blade.php
+++ resources/views/patrimonio/index.blade.php
@@ -3,34 +3,7 @@
 @section('content')
     <link rel="stylesheet" href="/css/patrimonio.css">
 
-    <div class="my-5 col-md-8 mx-auto">
-        <div class="d-flex align-items-center mb-2">
-            <h3>Patrimônio</h3>
-            <a href="{{ route('patrimonio.create') }}" style="margin-left: 5px;">
-                <img src="{{ asset('assets/plus-circle-fill.svg') }}" alt="Ícone de Adição" style="width: 30px; height: 30px;">
-            </a>
-        </div>
-
-        <div class="d-flex justify-content-center align-items-center">
-            <div class="flex-fill">
-                <form class="mb-0" action="{{ route('patrimonio.busca.get') }}" method="get">
-                    <div class="input-group">
-                        <input class="form-control" type="text" name="busca" id="busca"
-                            placeholder="Pesquisar por nome">
-                        <button style="background-color: #1A2876" class="btn" type="submit">
-                            <img src="{{ asset('images/busca.png') }}" alt="">
-                        </button>
-                    </div>
-                </form>
-            </div>
-            <div class="ms-3">
-                <button style="background-color: transparent; border: none; display: flex; align-items: center;">
-                    <img src="{{ asset('assets/Vector.svg') }}" alt="Ícone de filtro" style="margin-right: 10px;">
-                    <span style="color: #1A2876;">Filtrar</span>
-                </button>
-            </div>
-        </div>
-    </div>
+    @include('layouts.components.searchbar', ['title' => 'Patrimônio', 'addButton' => route('patrimonio.create'), 'searchForm' => route('patrimonio.busca.get')])
 
     <div class="container">
         <div class="row justify-content-center">
