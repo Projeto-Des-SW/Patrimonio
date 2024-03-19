f2e657b8458925df0fced0a8d660b0ac7816dee5
Rener1 <rener.tome123@gmail.com>
2024-03-19 11:42:20 -0300

    Removendo scripts não utilizados.

--- resources/views/patrimonio/index.blade.php
+++ resources/views/patrimonio/index.blade.php
@@ -84,9 +84,6 @@
 @endsection
 
 @push('scripts')
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="{{ asset('js/script.js') }}"></script>
-
     <script>
         function exibirModal() {
             $('#myModal').modal('show');
